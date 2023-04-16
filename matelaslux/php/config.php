53abb2e196c6272afe70c70b01c7e6fea5d3b299
Sifaqes Zerrouki <64240372+sifaqes@users.noreply.github.com>
2023-04-17 00:21:57 +0200

    Mockup tarjeta de visita el boss inmobiliaria

--- matelaslux/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $hostname = 'localhost';
-    $database = 'matelaslux';
-    $username = 'root';
-    $password = '';
-    $database = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8",$username,$password);
-    ?>
